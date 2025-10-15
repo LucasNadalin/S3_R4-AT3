c59816887d12d5513c9f78f9e9f0056423900404
LucasNadalin <lucasnfrancisco@gmail.com>
2025-10-14 22:47:52 -0300

    Desenvolvendo Ex1

--- app.js
+++ app.js
@@ -2,18 +2,46 @@ const express = require("express");
 const app = express();
 const PORT = 8082;
 
-app.get("/adicao/:numUm/:numDois", (req, res) => {
+app.get("/:operacao/:numUm/:numDois", (req, res) => {
     try {
-        const { numUm, numDois } = req.params;
-        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(numUm, numDois)){
-            return res.status(400).send(`Número Inválido`);
+        const{operacao, numUm, numDois} = req.params;
+
+        const n1 = parseFloat(numUm);
+        const n2 = parseFloat(numDois);
+
+        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(n1) || isNaN(n2)){
+            return res.status(400).send(`Caractere inválido ou faltanta, digite um número!`);
         }
-        const adicao = parseFloat(numUm) + parseFloat(numDois);
-        res.status(200).send(adicao);
+
+        let resultado
+
+        //Switch Case
+        switch(operacao){
+            case "adicao":
+                resultado = n1 + n2;
+            break;
+            case "subtracao":
+                resultado = n1 - n2;
+            break;
+            case "multiplicacao":
+                resultado = n1 * n2;
+            break;
+            case "divisao":
+                if (n2 === 0){
+                    return res.status(400).send("Não é possível fazer divisão por 0");
+                }else{
+                    resultado = n1 / n2;
+                }
+            break;
+            default:
+                return res.status(400).send("Operação inválida ou indispónivel");
+        }
+
+        res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);
 
 
     } catch (error) {
-        console.error("Erro ao realizar a adicao" + error)
+        console.error("Erro ao realizar o calculo!")
         res.status(500).send(`Erro interno no servidor!`)
     }
 });

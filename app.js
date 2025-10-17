184fdd33b685d3ae3d3140db4cc4130cb7fefb86
LucasNadalin <lucasnfrancisco@gmail.com>
2025-10-16 21:32:45 -0300

    Desenvolvendo Ex1

--- app.js
+++ app.js
@@ -2,50 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8082;
 
-app.get("/:operacao/:numUm/:numDois", (req, res) => {
-    try {
-        const{operacao, numUm, numDois} = req.params;
 
-        const n1 = parseFloat(numUm);
-        const n2 = parseFloat(numDois);
+function operacao(numUm, numDois){
+     try {
+        const numUm = parseFloat(req.params.numUm);
+        const numDois = parseFloat(req.params.numDois);
 
-        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(n1) || isNaN(n2)){
+        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(numUm) || isNaN(numDois)){
             return res.status(400).send(`Caractere inválido ou faltante, digite um número!`);
-        }
-
-        let resultado
-
-        //Switch Case
-        switch(operacao){
-            case "adicao":
-                resultado = n1 + n2;
-            break;
-            case "subtracao":
-                resultado = n1 - n2;
-            break;
-            case "multiplicacao":
-                resultado = n1 * n2;
-            break;
-            case "divisao":
-                if (n2 === 0){
-                    return res.status(400).send("Não é possível fazer divisão por 0");
-                }else{
-                    resultado = n1 / n2;
-                }
-            break;
-            default:
-                return res.status(400).send("Operação inválida ou indisponível");
-        }
-
-        res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);
-
+        };
 
     } catch (error) {
         console.error("Erro ao realizar o calculo!" + error)
         res.status(500).send(`Erro interno no servidor!`)
     }
-});
+};
+
+app.get("/soma/:numUm/:numDois", (req, res) => {});
+app.get("/subtracao/:numUm/:numDois", (req, res) => {});
+app.get("/multiplicacao/:numUm/:numDois", (req, res) => {});
+app.get("/divisao/:numUm/:numDois", (req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando http://localhost:${PORT}`)
 });
+
+
+       //let resultado
+
+       // res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);

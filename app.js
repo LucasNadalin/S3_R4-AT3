61daf1ac867df6ae997fa7af4659053d86fb5a63
LucasNadalin <lucasnfrancisco@gmail.com>
2025-10-16 21:43:48 -0300

    Desenvolvendo ex1

--- app.js
+++ app.js
@@ -2,32 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 8082;
 
-
-function operacao(numUm, numDois){
-     try {
+try {
+    function operacao(req, res) {
         const numUm = parseFloat(req.params.numUm);
         const numDois = parseFloat(req.params.numDois);
 
-        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(numUm) || isNaN(numDois)){
+        if (numUm & numDois == undefined || numUm & numDois == "" || isNaN(numUm) || isNaN(numDois)) {
             return res.status(400).send(`Caractere inválido ou faltante, digite um número!`);
         };
+    };
 
-    } catch (error) {
-        console.error("Erro ao realizar o calculo!" + error)
-        res.status(500).send(`Erro interno no servidor!`)
-    }
-};
+} catch (error) {
+    console.error("Erro ao realizar o calculo!" + error)
+    res.status(500).send(`Erro interno no servidor!`)
+}
 
-app.get("/soma/:numUm/:numDois", (req, res) => {});
-app.get("/subtracao/:numUm/:numDois", (req, res) => {});
-app.get("/multiplicacao/:numUm/:numDois", (req, res) => {});
-app.get("/divisao/:numUm/:numDois", (req, res) => {});
+app.get("/soma/:numUm/:numDois", (req, res) => { });
+app.get("/subtracao/:numUm/:numDois", (req, res) => { });
+app.get("/multiplicacao/:numUm/:numDois", (req, res) => { });
+app.get("/divisao/:numUm/:numDois", (req, res) => { });
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando http://localhost:${PORT}`)
 });
 
 
-       //let resultado
+//let resultado
 
-       // res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);
+// res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);

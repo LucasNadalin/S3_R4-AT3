a9376663840ec9bfedb0a53101eed0ff4fd9625a
LucasNadalin <lucasnfrancisco@gmail.com>
2025-10-17 21:10:49 -0300

    Desenvolvendo Ex3

--- app.js
+++ app.js
@@ -2,42 +2,43 @@ const express = require("express");
 const app = express();
 const PORT = 8083;
 
-app.get("/operacao/:tipo", (req, res) =>{
+app.get("/operacao/:tipo", (req, res) => {
     try {
         const tipo = req.params.tipo;
-        const numUm = parseFloat(req.query.numUm);
-        const numDois = parseFloat(req.query.numDois);
-
-        if(numUm & numDois == undefined || numUm & numDois == "" || isNaN(numUm) || isNaN(numDois)){
-            return res.status(400).send(`Caractere inválido ou faltante, digite um número!`);
-        }
-
-        let resultado;
+        const { numUm, numDois } = req.query;
 
         // Switch Case
-        switch(tipo){
+        switch (tipo) {
             case "soma":
-                resultado = numUm + numDois;
-            break;
-            case "subtracao":
-                resultado = numUm - numDois;
-            break;
-            case "multiplicacao":
-                resultado = numUm * numDois;
-            break;
-            case "divisao":
-                 if (numDois === 0){
-                    return res.status(400).send("Não é possível fazer divisão por 0");
-                }else{
-                    resultado = numUm / numDois;
+                if (numUm == undefined || numUm == "" || numDois == undefined || numDois == "" || isNaN(numUm) || isNaN(numDois)) {
+                    return res.status(400).send(`Caractere inválido, faltante ou tentativa de divisao por 0`);
                 };
-            break;
+                resultado = parseFloat(numUm) + parseFloat(numDois);
+                break;
+            case "subtracao":
+                if (numUm == undefined || numUm == "" || numDois == undefined || numDois == "" || isNaN(numUm) || isNaN(numDois)) {
+                    return res.status(400).send(`Caractere inválido, faltante ou tentativa de divisao por 0`);
+                };
+                resultado = parseFloat(numUm) - parseFloat(numDois);
+                break;
+            case "multiplicacao":
+                if (numUm == undefined || numUm == "" || numDois == undefined || numDois == "" || isNaN(numUm) || isNaN(numDois)) {
+                    return res.status(400).send(`Caractere inválido, faltante ou tentativa de divisao por 0`);
+                };
+                resultado = parseFloat(numUm) * parseFloat(numDois);
+                break;
+            case "divisao":
+                if (numUm == undefined || numUm == "" || numDois == undefined || numDois == "" || isNaN(numUm) || isNaN(numDois) || numDois == 0) {
+                    return res.status(400).send(`Caractere inválido, faltante ou tentativa de divisao por 0`);
+                };
+                resultado = parseFloat(numUm) / parseFloat(numDois);
+                break;
             default:
                 return res.status(400).send("Operação inválida ou indisponível");
         };
 
         res.status(200).send(`Resultado: ${resultado.toFixed(2)}`);
-        
+
     } catch (error) {
         console.error("Erro ao realizar o calculo!" + error);
         res.status(500).send(`Erro interno no servidor!`);
